d2fd5ce83b4b888acfc1f022dd81d286b2c9b74c
Pippa <pip.thompson96@gmail.com>
2021-05-31 19:05:31 +0100

    Add reset all tasks feature

--- script.js
+++ script.js
@@ -3,10 +3,31 @@ const dateEl = document.getElementById('date');
 let date = new Date().toDateString();
 dateEl.innerText = date;
 
+let resetButton = document.querySelector('.reset-button');
 
-//
+// ADD STRIKE THROUGH ON COMPLETED TASKS
 document.querySelector('body').addEventListener('click', (event) => {
     if (event.target.tagName === 'INPUT') {
         event.target.previousElementSibling.classList.toggle('strike-through');
     }
+})
+
+
+// REMOVE CLASS
+const resetElement = (task) => {
+    task.classList.remove('strike-through')
+}
+
+// UNCHECK BOX
+const resetCheckbox = (checkbox) => {
+    checkbox.checked = false;
+}
+
+// RESET ALL TASkS
+resetButton.addEventListener('click', function() {
+    let tasks = document.querySelectorAll('p');
+    tasks.forEach(task => resetElement(task) )
+
+    let checkboxes = document.querySelectorAll('input')
+    checkboxes.forEach(checkbox => resetCheckbox(checkbox))
 })
